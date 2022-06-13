ba41da23f095ebc170c738e8e17c0846a645df8d
robajillo <rjillo@yahoo.com>
2022-06-13 19:13:14 +0300

    update

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for main School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s[10] = "123456";
+
+printf("%s\n", s);
+rev_string(s);
+printf("%s\n", s);
+return (0);
+}
